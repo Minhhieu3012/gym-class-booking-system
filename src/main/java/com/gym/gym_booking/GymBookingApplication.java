b2b82e875e4de5adc9d8ffa95d5bacd3795ae58e
GiaBaoo <ngbao840@gmail.com>
2026-09-05 02:22:07 +0700

    initialize Spring Boot project

---
+++ src/main/java/com/gym/gym_booking/GymBookingApplication.java
@@ -0,0 +1,13 @@
+package com.gym.gym_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymBookingApplication.class, args);
+	}
+
+}
