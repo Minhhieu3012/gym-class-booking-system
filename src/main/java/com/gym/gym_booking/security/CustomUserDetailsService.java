2e83cd11deb2de5941d5a558c72d58bd819063be
GiaBaoo <ngbao840@gmail.com>
2026-09-08 20:59:56 +0700

    config for security & JWT & hash password

--- src/main/java/com/gym/gym_booking/security/CustomUserDetailsService.java
+++ src/main/java/com/gym/gym_booking/security/CustomUserDetailsService.java
@@ -1,4 +1,38 @@
 package com.gym.gym_booking.security;
 
-public class CustomUserDetailsService {
+import com.gym.gym_booking.entity.User;
+import com.gym.gym_booking.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository
+                .findByPhoneOrEmail(username, username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .disabled(user.getStatus().name().equals("PENDING")
+                        || user.getStatus().name().equals("REJECTED"))
+                .accountLocked(user.getStatus().name().equals("LOCKED"))
+                .build();
+    }
 }
